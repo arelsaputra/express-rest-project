9258fe96ef1957c6dd28025951e60e064c3d6864
yfchiuaa <atomcharliezrf@gmail.com>
2021-03-02 23:46:47 +0800

    add swagger ui

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./database');
 const compression = require('compression');
+var swaggerJsdoc = require('swagger-jsdoc');
+var swaggerUi = require('swagger-ui-express');
 require('dotenv').config();
 
 const app = express();
@@ -34,6 +36,34 @@ app.use((req, res, next) => {
 
 // Export app for testing
 module.exports = app;
+
+// Set up swagger ui
+const options = {
+    definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express-rest API Swagger Documentation",
+      version: "0.1.0",
+      description: "The swagger doc for example api endpoints in this express-rest template",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Charlie Chiu",
+        url: "https://github.com/yfchiuaa",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:8080/api/user",
+      },
+    ],
+  },
+  apis: ["./routes/UserRoutes.js"],
+}
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Start server
 const PORT = process.env.PORT || 3000;
