ce81f23eb94e2a028c9f6912cd7c3db6d9b02878
arelsaputra <saputraarel@gmail.com>
2025-08-07 18:57:45 +0700

    Temp disable DB for testing Cloud Run

--- database.js
+++ database.js
@@ -1,13 +1,11 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config()
+require('dotenv').config();
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idel: 10000
-    },
-    // Display all log function call parameter
-    // logging: (...msg) => console.log(msg),
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres', // ganti ke mysql jika pakai mysql
+  logging: false,
 });
+
+module.exports = sequelize;
